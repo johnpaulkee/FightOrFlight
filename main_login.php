3be9740c7694edc49447e79a967e9a723e67ca19
Mohammad Aziz <i2u9a@deas.ugrad.cs.ubc.ca>
2015-06-11 19:49:30 -0700

    Login sorta works- need db to test

--- main_login.php
+++ main_login.php
@@ -5,7 +5,7 @@
 <table class="table table-striped"> 
 <tablewidth="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
 <tr>
-<form name="form1" method="post" action="checklogin.php">
+<form name="form1" method="post" action="check_login.php">
 <td>
 <table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
 <tr>
