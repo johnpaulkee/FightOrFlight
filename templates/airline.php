508d529e52a35793b62f7352aa282268d41d021c
John Paul Kee <johnpaulkee@gmail.com>
2015-06-11 18:32:27 -0700

    updated index

--- templates/airline.php
+++ templates/airline.php
@@ -1,77 +1,36 @@
 <html>
+<?php include 'header.php'; ?>
+<body>
 
-  <head>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-  </head>
-
-  <body>
-    <?php include "navbar.php" ;?>
-          <!-- Menu icon -->
-      <div class="icon-close">
-        <img src="http://s3.amazonaws.com/codecademy-content/courses/ltp2/img/uber/close.png">
-      </div>
-
-      <!-- Menu -->
   <div class="menu">
-        <ul>
-          <li><a href="#">Sell Ticket</a></li>
-           <li><a href="#">Customer Details</a></li>
-           <li><a href="#">Airline Details</a></li>
-           <li><a href="#">Aircraft Details</a></li>    
-           <li><a href="#">Customer Baggage Details</a></li>    
-           <li><a href="#">Discount Details</a></li>    
-           <li><a href="#">Purchase Discounted Ticket</a></li>  
-           <li><a href="#">Purchased Ticket Details</a></li>    
-           <li><a href="#">Able to all the Planes owned by an Airline</a></li>    
-           <li><a href="#">Airline Headquarter Location</a></li>    
-           <li><a href="#">Lookup Boarding Pass Details</a></li>    
-       </ul>
-        <ul>
-          <li><a href="#">Purchase Ticket</a></li>
-           <li><a href="#">Become a Frequent Flyer</a></li>
-           <li><a href="#">Check Points</a></li>
-           <li><a href="#">Purchased Tickets Details</a></li>   
-           <li><a href="#">Are you blacklisted?</a></li>    
-           <li><a href="#">Purchased Tickets Details</a></li>   
-           <li><a href="#">Update your Credit Card Information</a></li> 
-           <li><a href="#">Check Luggage Weight</a></li>    
-           <li><a href="#">Lookup Boarding Pass Details</a></li>    
-           <li><a href="#">Check Airline Headquarters</a></li>    
-           <li><a href="#">Check Plane Details</a></li>   
-           <li><a href="#">Purchased Tickets Details </a></li>    
-       </ul>
-      </div>
+    
+    <!-- Menu icon -->
+    <div class="icon-close">
+      <img src="http://s3.amazonaws.com/codecademy-content/courses/ltp2/img/uber/close.png">
+    </div>
 
-        <form class="navbar-form navbar-left" role="search">
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Search">
-          </div>
-          <button type="submit" class="btn btn-default">Submit</button>
-        </form>
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="#">Link</a></li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-            <ul class="dropdown-menu" role="menu">
-              <li><a href="#">Action</a></li>
-              <li><a href="#">Another action</a></li>
-              <li><a href="#">Something else here</a></li>
-              <li class="divider"></li>
-              <li><a href="#">Separated link</a></li>
-            </ul>
-          </li>
-        </ul>
-      </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-  </nav>
+    <!-- Menu -->
+    <ul>
+      <li><a href="#">  Add Tickets so that customers can purchase them </a> </li>
+      <li><a href="#">  Distribute boarding passes </a> </li>
+      <li><a href="#">  Check up details on their Airline Employees </a> </li>
+      <li><a href="#">  Assign discounts for each airline employee </a> </li>
+      <li><a href="#">  Update Customer’s Frequent Flyer Points </a> </li>
+      <li><a href="#"> Join an Alliance </a> </li>
+      <li><a href="#"> Choose the country that they want their headquarters in </a> </li>
+      <li><a href="#"> Lookup Airport Details </a> </li>
+    </ul>
+  </div>
 
-  <?php include 'oracle-test.php'; ?>
+  <!-- Main body -->
+  <div class="jumbotron">
 
+    <div class="icon-menu">
+      <i class="fa fa-bars"></i>
+      Menu
+    </div>
+    
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="../js/app.js"></script>
   </body>
-
-</html>
+  </html>
