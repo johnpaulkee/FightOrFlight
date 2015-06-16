b4e0affb69b88e6af9a13d1ef2eeb794f600b73d
Kamil Khan <kamil.z.khan@gmail.com>
2015-06-16 15:10:14 -0700

    fixing search query

--- templates/customer_templates/check_headquarters.php
+++ templates/customer_templates/check_headquarters.php
@@ -28,43 +28,6 @@ function executePlainSQL($cmdstr) {
   return $statement;
 }
 
-function executeBoundSQL($cmdstr, $list) {
-/* Sometimes a same statement will be excuted for severl times, only
-the value of variables need to be changed.
-In this case you don't need to create the statement several times; 
-using bind variables can make the statement be shared and just 
-parsed once. This is also very useful in protecting against SQL injection. See example code below for       how this functions is used */
-
-global $db_conn, $success;
-$statement = OCIParse($db_conn, $cmdstr);
-
-if (!$statement) {
-  echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
-  $e = OCI_Error($db_conn);
-  echo htmlentities($e['message']);
-  $success = False;
-}
-
-foreach ($list as $tuple) {
-  foreach ($tuple as $bind => $val) {
-//echo $val;
-//echo "<br>".$bind."<br>";
-    OCIBindByName($statement, $bind, $val);
-unset ($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
-
-}
-$r = OCIExecute($statement, OCI_DEFAULT);
-if (!$r) {
-  echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-$e = OCI_Error($statement); // For OCIExecute errors pass the statementhandle
-echo htmlentities($e['message']);
-echo "<br>";
-$success = False;
-}
-}
-
-}
-
 function printResult($result) { //prints results from a select statement
   echo "<h3><center> Here are the available tickets that you can purchase: </center></h3>";
 
@@ -83,8 +46,6 @@ function printResult($result) { //prints results from a select statement
     echo "<td>" . $row[0] . "</td>";
     echo "<td>" . $row[1] . "</td>";
     echo "<td>" . $row[2] . "</td>";
-    echo "<td>" . $row[3] . "</td>";
-    echo "<td>" . $row[4] . "</td>";
     echo "</tr>";
   }
   echo "</tbody>";
@@ -94,12 +55,11 @@ function printResult($result) { //prints results from a select statement
 
 // Connect Oracle...
 if ($db_conn) {
-  $result = executePlainSQL("SELECT airline_code, airline_name, name from Airline_Headquartered_In where airline_name LIKE'".$airline."'");
+  $result = executePlainSQL("SELECT airline_code, airline_name, name from Airline_Headquartered_In where airline_name LIKE'%".$airline."%'");
   printResult($result);
 }
 
 //Commit to save changes...
-OCILogoff($db_conn);
 } else {
   echo "cannot connect";
 $e = OCI_Error(); // For OCILogon errors pass no handle
