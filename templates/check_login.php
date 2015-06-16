f14a4a3995db16a4b36dffbf99e348269c9fb2af
Kamil Khan <kamil.z.khan@gmail.com>
2015-06-15 22:15:42 -0700

    testing

--- templates/check_login.php
+++ templates/check_login.php
@@ -12,7 +12,9 @@
 			//echo "<br>running ".$cmdstr."<br>";
 			$db_con = OCILogon("ora_i4u9a", "a34129122", "ug");
 			$statement = OCIParse($db_con, $cmdstr); //There is a set of comments at the end of the file that describe some of the OCI specific functions and how they work
-
+			echo "Hello";
+			echo $statement;
+			echo "goodbye";
 			if (!$statement) {
 				echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
 				$e = OCI_Error($db_conn); // For OCIParse errors pass the       
@@ -29,7 +31,7 @@
 				$success = False;
 			} else {}
 			return $statement;
-		}
+	}
 
 	if($type == "customer") {
 		$table = "Customer_Login";
