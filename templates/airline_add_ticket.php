8ee7f27e49d036300d101b01b844fd1e5d18aa1a
Kamil Khan <kamil.z.khan@gmail.com>
2015-06-15 18:25:18 -0700

    debugging

--- templates/airline_add_ticket.php
+++ templates/airline_add_ticket.php
@@ -3,13 +3,35 @@
 	<label> Select Plane </label>
 	<select class="form-control">
 		<?php
-		$bd_conn = OCILogon("ora_i4u9a", "a34129122", "ug");
+			function executePlainSQL($cmdstr) { 
+			//echo "<br>running ".$cmdstr."<br>";
+			global $db_conn, $success;
+			$statement = OCIParse($db_conn, $cmdstr); //There is a set of comments at the end of the file that describe some of the OCI specific functions and how they work
+
+			if (!$statement) {
+				echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+				$e = OCI_Error($db_conn); // For OCIParse errors pass the       
+				// connection handle
+				echo htmlentities($e['message']);
+				$success = False;
+			}
+
+			$r = OCIExecute($statement, OCI_DEFAULT);
+			if (!$r) {
+				echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+				$e = oci_error($statement); // For OCIExecute errors pass the statementhandle
+				echo htmlentities($e['message']);
+				$success = False;
+			} else {}
+			return $statement;
+		}
+
+		$bd_conn = OCILogon("ora_u8a9", "a32101131", "ug");
 		$query = "SELECT *
-				  FROM Airline_Headquartered_In a";
-		$statement = OCIParse($db_conn, $query);
-		$result = OCIExecute($statement, OCI_DEFAULT);
+				  FROM Airline_Headquartered_In";
+		$result = executePlainSQL($query, $result);
 		while(($row = oci_fetch_row($statement)) != false) {
-			$option = '<option value="'.$row[3].$row[0].'">'.$row[0].$row[1].$row[2].'</option>';
+			$option = '<option value="'.$row[2].$row[0].'">'.$row[0].$row[1].$row[2].'</option>';
 			echo($option);
 		}
 		oci_free_statement($statement);
