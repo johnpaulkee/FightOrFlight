2a0f6081fb7ba9e99c7bd903b7320825d374638f
Kamil Khan <kamil.z.khan@gmail.com>
2015-06-16 18:41:18 -0700

    added add_ticket logic

--- templates/airline_templates/add_ticket.php
+++ templates/airline_templates/add_ticket.php
@@ -1,5 +1,4 @@
 <?php
-require('eci_query_header.php');
 $success = True; //keep track of errors so it redirects the page only if there are no errors
 $db_conn = OCILogon("ora_i4u9a", "a34129122", "ug");
 
@@ -39,6 +38,29 @@ function printResult($result) { //prints results from a select statement
   echo $result;
 
 }
+
+function executePlainSQL($cmdstr) { 
+      //echo "<br>running ".$cmdstr."<br>";
+      $db_con = OCILogon("ora_i4u9a", "a34129122", "ug");
+      $statement = OCIParse($db_con, $cmdstr); //There is a set of comments at the end of the file that describe some of the OCI specific functions and how they work
+
+      if (!$statement) {
+        echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+        $e = OCI_Error($db_conn); // For OCIParse errors pass the       
+        // connection handle
+        echo htmlentities($e['message']);
+        $success = False;
+      }
+
+      $r = OCIExecute($statement, OCI_DEFAULT);
+      if (!$r) {
+        echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+        $e = oci_error($statement); // For OCIExecute errors pass the statementhandle
+        echo htmlentities($e['message']);
+        $success = False;
+      } else {}
+      return $statement;
+    }
 
 // Connect Oracle...
 if ($db_conn) {
