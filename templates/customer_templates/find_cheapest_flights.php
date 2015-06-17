071f3d6fcf1cf4e8e21eda6f790fa8ea6cb9c62a
Kamil Khan <kamil.z.khan@gmail.com>
2015-06-17 03:26:14 -0700

    fixing merge conflicts

--- templates/customer_templates/find_cheapest_flights.php
+++ templates/customer_templates/find_cheapest_flights.php
@@ -55,22 +55,16 @@ function printResult($result) { //prints results from a select statement
 
 // Connect Oracle...
 if ($db_conn) {
-	$view_query = "CREATE VIEW outbound_tickets as
-					SELECT t1.tID 
-					FROM Ticket t1, Comprised_Of c1, Airport_LocatedIn a1 
-					WHERE t1.tID = c1.tID AND c1.from_airport_code = '".$airport."'";
+	$view_query = "CREATE VIEW outbound_tickets as SELECT t1.tID FROM Ticket t1, Comprised_Of c1 WHERE t1.tID = c1.tID AND c1.from_airport_code = '".$airport."'";
 	$result1 = executePlainSQL($view_query);
-	// $query = "SELECT MIN(t.price) as minPrice, t.tID 
-	// FROM Ticket t, outbound_tickets o
-	// WHERE t.tID = o.tID";
-	// $result2 = executePlainSQL($query);
-	// echo $result1;
-	// echo $result2;
+	$query = "SELECT MIN(t.price) as minPrice, t.tID FROM Ticket t, outbound_tickets o WHERE t.tID = o.tID";
+	$result2 = executePlainSQL($query);
+	echo $result1;
+	echo $result2;
 	// while(($row = oci_fetch_row($result)) != false){
 	// 	echo "<p>".$row[0].", ".$row[1].", ".$row[2].", ".$row[4]."</p>";
 	// 	echo "<br>";
 	// }
-	}
 }
 
 ?>
