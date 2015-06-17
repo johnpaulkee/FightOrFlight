f167568e782b5aee7c4c80a728bd34ad3db816a4
Kamil Khan <kamil.z.khan@gmail.com>
2015-06-16 18:43:22 -0700

    added add_ticket logic

--- templates/airline_templates/add_ticket.php
+++ templates/airline_templates/add_ticket.php
@@ -1,1 +1,17 @@
-<p>test</p>
+<?php
+$success = True; //keep track of errors so it redirects the page only if there are no errors
+$db_conn = OCILogon("ora_i4u9a", "a34129122", "ug");
+
+
+  // Define user and pass
+$capacity = ucfirst($_POST['plane']);
+$price = ucfirst($_POST['price']);
+$first_num = ucfirst($_POST['first_class']);
+$first_price = ucfirst($_POST['first_class_price']);
+$business_num = ucfirst($_POST['business']);
+$business_price = ucfirst($_POST['business_price']);
+$economy = $capacity - $first_num - $business_num;
+
+
+
+?>
