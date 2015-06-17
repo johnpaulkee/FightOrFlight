9322133ac6782b5ec688214c9fcd3e0b13652910
Kamil Khan <kamil.z.khan@gmail.com>
2015-06-16 18:44:29 -0700

    added add_ticket logic

--- templates/airline_templates/add_ticket.php
+++ templates/airline_templates/add_ticket.php
@@ -12,6 +12,65 @@ $business_num = ucfirst($_POST['business']);
 $business_price = ucfirst($_POST['business_price']);
 $economy = $capacity - $first_num - $business_num;
 
+function printResult($result) { //prints results from a select statement
+  echo "<h3><center> Here are the available tickets that you can purchase: </center></h3>";
 
+  echo "<table class = 'table table-striped'>";
+  echo "<thead>";
+  echo "<tr>";
+  echo "<th>tID</th>";
+  echo "<th>Seat</th>";
+  echo "<th>Class</th>";
+  echo "<th>Price</th>";
+  echo "</tr>";
+  echo "</thead>";
+  echo "<tbody>";
+  while (($row = oci_fetch_row($result)) != false) {
+    echo "<tr>";
+    echo "<td>" . $row[0] . "</td>";
+    echo "<td>" . $row[1] . "</td>";
+    echo "<td>" . $row[2] . "</td>";
+    echo "<td>" . $row[3] . "</td>";
+    echo "</tr>";
+  }
+  echo "</tbody>";
+  echo "</table>";
+  echo $result;
+
+}
+
+function executePlainSQL($cmdstr) { 
+      //echo "<br>running ".$cmdstr."<br>";
+      $db_con = OCILogon("ora_i4u9a", "a34129122", "ug");
+      $statement = OCIParse($db_con, $cmdstr); //There is a set of comments at the end of the file that describe some of the OCI specific functions and how they work
+
+      if (!$statement) {
+        echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+        $e = OCI_Error($db_conn); // For OCIParse errors pass the       
+        // connection handle
+        echo htmlentities($e['message']);
+        $success = False;
+      }
+
+      $r = OCIExecute($statement, OCI_DEFAULT);
+      if (!$r) {
+        echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+        $e = oci_error($statement); // For OCIExecute errors pass the statementhandle
+        echo htmlentities($e['message']);
+        $success = False;
+      } else {}
+      return $statement;
+    }
+
+// Connect Oracle...
+if ($db_conn) {
+  $k = 0;
+  $s1 = 0;
+  $s2 = "A";
+  $s3 = 0;
+  $query = "SELECT * FROM Tickets";
+  $result = executePlainSQL($query);
+  printResult($result);
+}
 
 ?>
