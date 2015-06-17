71695df8970cc5ef083b4b42b0742e81aa410ed2
Kamil Khan <kamil.z.khan@gmail.com>
2015-06-17 03:53:33 -0700

    fixing query

--- templates/customer_templates/find_cheapest_flights.php
+++ templates/customer_templates/find_cheapest_flights.php
@@ -60,7 +60,7 @@ if ($db_conn) {
 	$result2 = executePlainSQL($drop_narrowed_view);
 	$create_view = "CREATE VIEW cheap_tickets AS SELECT MIN(t.price) as minPrice, t.tID FROM Ticket t, outbound_tickets o WHERE t.tID = o.tID GROUP BY t.tID";
 	$result3 = executePlainSQL($create_view);
-	$query = "SELECT t.tID FROM Ticket t, Comprised_Of c WHERE t.tID = c.tID AND c.from_aiport_code = '".$airport."' AND t.price = (SELECT MIN(minPrice) FROM cheap_tickets c)";
+	$query = "SELECT t.tID FROM Ticket t, Comprised_Of c WHERE t.tID = c.tID AND c.from_airport_code = '".$airport."' AND t.price = (SELECT MIN(minPrice) FROM cheap_tickets c)";
 	$result4 = executePlainSQL($query);
 	printResult($result4);
 	// while(($row = oci_fetch_row($result)) != false){
