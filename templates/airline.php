a0b418823939764391ff0652f21c3abb6e826006
Kamil Khan <kamil.z.khan@gmail.com>
2015-06-17 22:46:03 -0700

    done?git add -Agit add -A :))))

--- templates/airline.php
+++ templates/airline.php
@@ -43,7 +43,34 @@ $type = $_COOKIE['type'];
       ?>
       </div>
       <div class="panel-body scroller" id="form_generation">
-        <p> TODO: include airline details </p>
+        <?php 
+        function executePlainSQL($cmdstr) { 
+      //echo "<br>running ".$cmdstr."<br>";
+      $db_con = OCILogon("ora_i4u9a", "a34129122", "ug");
+      $statement = OCIParse($db_con, $cmdstr); //There is a set of comments at the end of the file that describe some of the OCI specific functions and how they work
+
+      if (!$statement) {
+        echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+        $e = OCI_Error($db_conn); // For OCIParse errors pass the       
+        // connection handle
+        echo htmlentities($e['message']);
+        $success = False;
+      }
+
+      $r = OCIExecute($statement, OCI_DEFAULT);
+      if (!$r) {
+        echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+        $e = oci_error($statement); // For OCIExecute errors pass the statementhandle
+        echo htmlentities($e['message']);
+        $success = False;
+      } else {}
+      return $statement;
+    }
+          $query = "SELECT airline_name FROM Airline_Headquartered_In WHERE airline_code =".$_COOKIE['id'];
+          $result = executePlainSQL($query);
+          $row = oci_fetch_row($result);
+          echo "<p> Hello, ".$row[0]."</p>";
+        ?>
       </div>
       </div>
     </div>
