8fc367bc9d7067fcf164accf3f6c20490b0ddff6
Kamil Khan <kamil.z.khan@gmail.com>
2015-06-17 22:32:04 -0700

    done?git add -Agit add -A :))))

--- templates/airline_templates/airport_details_form.php
+++ templates/airline_templates/airport_details_form.php
@@ -30,32 +30,6 @@ $db_conn = OCILogon("ora_i4u9a", "a34129122", "ug");
 </form>
 
 <?php
-function printResult($result) { //prints results from a select statement
-  echo "<h3><center> Here are the details for all the airports: </center></h3>";
-
-  echo "<table class = 'table table-striped'>";
-  echo "<thead>";
-  echo "<tr>";
-  echo "<th>Airport Code</th>";
-  echo "<th>Number of Domestic Gates</th>";
-  echo "<th>City</th>";
-  echo "<th>Country</th>";
-  echo "</tr>";
-  echo "</thead>";
-  echo "<tbody>";
-  while (($row = oci_fetch_row($result)) != false) {
-    echo "<tr>";
-    echo "<td>" . $row[0] . "</td>";
-    echo "<td>" . $row[1] . "</td>";
-    echo "<td>" . $row[2] . "</td>";
-    echo "<td>" . $row[3] . "</td>";
-    echo "</tr>";
-  }
-  echo "</tbody>";
-  echo "</table>";
-  echo $result;
-
-}
 
 // Connect Oracle...
 if ($db_conn) {
@@ -63,7 +37,6 @@ if ($db_conn) {
   $result = executePlainSQL($query);
   echo $result;
   //echo $airline;
-  printResult($result);
 }
 ?>
 
@@ -72,7 +45,7 @@ if ($db_conn) {
 <script>
 
   
-  $("#details").submit(function() {
+  $("#getAirportDetails").submit(function() {
 
     var url = $(this).attr("action");
 
