983b71f56338a22483ff425312ee35ab6ef31b78
Kamil Khan <kamil.z.khan@gmail.com>
2015-06-17 19:32:15 -0700

    working on aircraft retirement

--- templates/airline_templates/retire_aircraft_form.php
+++ templates/airline_templates/retire_aircraft_form.php
@@ -36,7 +36,7 @@
       	$query = "SELECT * FROM Plane_Owned_By WHERE airline_code = ".$_COOKIE['id'];
       	$result = executePlainSQL($query);
       	while(($row = oci_fetch_row($result)) != false) {
-      		$input = '<input type="radio" name="plane" value="'.$row[0].','.$row[1].'">'.$row[1].', '.$row[2].', '.$row[3];
+      		$input = '<input type="radio" name="plane" value="'.$row[0].','.$row[1].'">'.$row[1].', '.$row[2].', '.$row[3].'<br>';
       		echo $input;
       	}
 	?>
